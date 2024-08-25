3622da68110fae09880e7232aef5e79ad64d3407
sharif-jobayed <sharif.jobayed@gmail.com>
2024-08-26 02:15:38 +0600

    Complete NavBar

--- vite-reactJS/src/components/SideNav.jsx
+++ vite-reactJS/src/components/SideNav.jsx
@@ -1,6 +1,8 @@
 
 import { useState } from 'react';
-import { AiOutlineHome, AiOutlineMenu } from 'react-icons/ai';
+import { AiOutlineHome, AiOutlineMenu, AiOutlineProject, AiOutlineMail } from 'react-icons/ai';
+import {GrProjects} from 'react-icons/gr';
+import {BsPerson} from 'react-icons/bs';
 
 const SideNav = () => {
 	const [nav, setNav] = useState(false);
@@ -22,19 +24,19 @@ const SideNav = () => {
 							<span className="pl-4">Home</span>
 						</a>
 						<a href="#" className="w-[75%] flex justify-center items-center rounded-full bg-gray-100 shadow-gray-200 shadow-md m-2 p-4 cursor-pointer hover:scale-110 ease-in-out duration-300">
-							<AiOutlineHome size={20} />
+							<GrProjects size={20} />
 							<span className="pl-4">Work</span>
 						</a>
 						<a href="#" className="w-[75%] flex justify-center items-center rounded-full bg-gray-100 shadow-gray-200 shadow-md m-2 p-4 cursor-pointer hover:scale-110 ease-in-out duration-300">
-							<AiOutlineHome size={20} />
+							<AiOutlineProject size={20} />
 							<span className="pl-4">Projects</span>
 						</a>
 						<a href="#" className="w-[75%] flex justify-center items-center rounded-full bg-gray-100 shadow-gray-200 shadow-md m-2 p-4 cursor-pointer hover:scale-110 ease-in-out duration-300">
-							<AiOutlineHome size={20} />
+							<BsPerson size={20} />
 							<span className="pl-4">Resume</span>
 						</a>
 						<a href="#" className="w-[75%] flex justify-center items-center rounded-full bg-gray-100 shadow-gray-200 shadow-md m-2 p-4 cursor-pointer hover:scale-110 ease-in-out duration-300">
-							<AiOutlineHome size={20} />
+							<AiOutlineMail size={20} />
 							<span className="pl-4">Contact</span>
 						</a>
 					</div>
